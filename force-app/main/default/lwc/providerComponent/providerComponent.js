420a2ddf5be9ae5dfa767da2dfffb04bad7f31a9
umar-7w4 <mohammadumar7w4@gmail.com>
2022-12-09 21:04:20 +0530

    Added my project

--- force-app/main/default/lwc/providerComponent/providerComponent.js
+++ force-app/main/default/lwc/providerComponent/providerComponent.js
@@ -1,18 +1,178 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import dataChannel2 from '@salesforce/messageChannel/DataChannel2__c';
+import GETPROVIDERS from "@salesforce/apex/Provider.getProviders";
 
 export default class ProviderComponent extends LightningElement {
 
-    value = 'Select gender';
+    valueGender = 'Select gender';
+    valueInNetwork = 'Select In-Network';
+    valueSpeciality = 'Select Speciality';
+    valueDistance = 'Select Distance';
 
-    get options() {
+    @api speciality;
+
+    @api firstName;
+    @api lastName;
+    @api gender;
+    @api startTime;
+    @api endTime;
+    @api inNetwork;
+    @api taxanomy;
+    @api location;
+    @api distance;
+
+    handleFirstName(event) {
+        this.firstName = event.detail.value;
+        //console.log(this.firstName);
+    }
+
+    handleLastName(event) {
+        this.lastName = event.detail.value;
+        //console.log(this.lastName);
+    }
+
+    handleGenderChange(event) {
+        this.gender = event.detail.value;
+        //console.log(this.gender);
+    }
+
+    handleInNetworkChange(event){
+        this.inNetwork = event.detail.value;
+        //console.log(this.inNetwork);
+    }
+
+    handleStartTime(event){
+        this.startTime = event.detail.value;
+       // console.log(this.startTime);
+    }
+
+    handleEndTime(event){
+        this.endTime = event.detail.value;
+        //console.log(this.endTime);
+    }
+
+    handleSpecialityChange(event){
+        //console.log(event.detail.value);
+        this.speciality = event.detail.value;
+        //JSON.parse(JSON.stringify(event.detail.value));
+        //console.log(this.selectedSpecialities);
+    }
+
+    handleTaxonomy(event){
+        this.taxanomy = event.detail.value;
+        //console.log(this.taxanomy);
+    }
+
+    handleLocation(event){
+        this.location = event.detail.value;
+        //console.log(this.location);
+    }
+
+    handleDistanceChange(event){
+        this.distance = event.detail.value;
+        //console.log(this.distance);
+    }
+
+    get optionsGender() {
         return [
-            { label: 'Select gender', value: 'Select gender' },
             { label: 'Male', value: 'Male' },
             { label: 'Female', value: 'Female' },
         ];
     }
 
-    handleGenderChange(event) {
-        this.value = event.detail.value;
+    get optionsInNetwork(){
+        return [
+            { label: 'In-Network', value: 'In-Network' },
+            { label: 'Out-of-Network', value: 'Out-of-Network' },
+        ];
     }
+
+    get optionsDistance(){
+        return [
+            { label: '10 Miles Away', value: '10 Miles Away' },
+            { label: '25 Miles Away', value: '25 Miles Away' },
+            { label: '50 Miles Away', value: '50 Miles Away' },
+            { label: '100 Miles Away', value: '100 Miles Away' },
+        ];
+    }
+    
+    get optionsSpeciality(){
+        return [
+            { label: 'Internal medicine', value: 'Internal medicine' },
+            { label: 'Anesthesiology', value: 'Anesthesiology' },
+            { label: 'Neurology', value: 'Neurology' },
+            { label: 'Orthopedics', value: 'Orthopedics' },
+            { label: 'Psychiatry', value: 'Psychiatry' },
+            { label: 'Pediatrics', value: 'Pediatrics' },
+            { label: 'Dermatology', value: 'Dermatology' },
+            { label: 'Family medicine', value: 'Family medicine' },
+            { label: 'Radology', value: 'Radology' },
+            { label: 'Pathaology', value: 'Pathaology' },
+            { label: 'General Surgery', value: 'General Surgery' },
+            { label: 'Oncology', value: 'Oncology' },
+            { label: 'Urology', value: 'Urology' },
+            { label: 'Nuclear medicine', value: 'Nuclear medicine' },
+            { label: 'Cardiology', value: 'Cardiology' },
+            { label: 'Orthopedics', value: 'Orthopedics' },
+            { label: 'Plastic surgery', value: 'Plastic surgery' },
+            { label: 'Neurosurgeon', value: 'Neurosurgeon' },
+            { label: 'Hematology', value: 'Hematology' },
+            { label: 'Endocrinology', value: 'Endocrinology' },
+            { label: 'Gerialtrics', value: 'Gerialtrics' },
+            { label: 'Vascular surgery', value: 'Vascular surgery' },
+            { label: 'Gynaecology', value: 'Gynaecology' },
+            { label: 'Neonatology', value: 'Neonatology' },
+            { label: 'Neurology', value: 'Neurology' },
+            { label: 'Pain management', value: 'Pain management' },
+        ];
+    }
+
+
+    @wire(MessageContext)
+    messageContext2;
+
+    providers = [];
+
+    handleSubmit(event){
+        var providerName;
+        if(this.lastName==undefined){
+            providerName = this.firstName;
+        }
+        else{
+            providerName = this.firstName+" "+this.lastName;
+        }
+        GETPROVIDERS({ 
+            name: providerName,
+            gender : this.gender, 
+            inNetwork : this.inNetwork,
+            taxonomy : this.taxanomy, 
+            startTime : this.startTime,
+            endTime : this.endTime, 
+            distance : this.distance,
+            specialities : this.selectedSpecialities, 
+            location : this.location
+        })
+        .then(result => {
+            this.providers = result;
+            this.resultLength = this.providers.length;
+            
+            console.log(providerName+" "+this.gender+" "+this.inNetwork+" "+this.taxanomy+" "+this.startTime+" "+this.endTime+" "+this.distance+this.location);
+            console.log(this.selectedSpecialities);
+            console.log(this.providers);
+
+            this.dispatchEvent(event);
+
+            const payload = { data: this.providers };
+            publish(this.messageContext2, dataChannel2, payload);
+            console.log("Payload"+payload);
+        })
+        .catch(error => {
+            console.log(error);
+            this.dispatchEvent(event);
+        });
+
+
+    }
+
 }
--- force-app/main/default/lwc/providerResult/providerResult.js
+++ force-app/main/default/lwc/providerResult/providerResult.js
@@ -1,3 +1,90 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api , wire} from 'lwc';
 
-export default class ProviderResult extends LightningElement {}
+import getAccounts from '@salesforce/apex/Patient.getPatientDetails';
+import { refreshApex } from '@salesforce/apex';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { publish } from 'lightning/messageService';
+
+import NAME from '@salesforce/schema/Provider__c.Provider_Name__c';
+import DISTANCE from '@salesforce/schema/Provider__c.Distance__c';
+import ENDTIME from '@salesforce/schema/Provider__c.End_Time__c';
+import GENDER from '@salesforce/schema/Provider__c.Gender__c';
+import INNETWORK from '@salesforce/schema/Provider__c.In_Network__c';
+import LOCATION from '@salesforce/schema/Provider__c.Location__c';
+import SPECIALITY from '@salesforce/schema/Provider__c.Speciality__c';
+import STARTTIME from '@salesforce/schema/Provider__c.Start_Time__c';
+import TAXONOMY from '@salesforce/schema/Provider__c.Taxonomy__c';
+
+import dataChannel from '@salesforce/messageChannel/DataChannel__c';
+import {
+    subscribe,
+    unsubscribe,
+    MessageContext
+} from 'lightning/messageService';
+
+const COLS = [
+
+    {
+        label: 'Provider Name',
+        fieldName: NAME.fieldApiName,
+        editable: true
+    },
+    {
+        label: 'Gender',
+        fieldName: GENDER.fieldApiName,
+        editable: true
+    },
+    { 
+        label: 'In Network', 
+        fieldName: INNETWORK.fieldApiName, 
+        editable: true 
+    },
+    {
+        label: 'Speciality',
+        fieldName: SPECIALITY.fieldApiName,
+        editable: true
+    },
+    { 
+        label: 'Taxonomy', 
+        fieldName: TAXONOMY.fieldApiName, 
+        editable: true 
+    },
+    { 
+        label: 'Start Time', 
+        fieldName: STARTTIME.fieldApiName, 
+        editable: true 
+    },
+    { 
+        label: 'End Time', 
+        fieldName: ENDTIME.fieldApiName, 
+        editable: true 
+    },
+    { 
+        label: 'Location', 
+        fieldName: LOCATION.fieldApiName, 
+        editable: true 
+    },
+    { 
+        label: 'Distance', 
+        fieldName: DISTANCE.fieldApiName, 
+        editable: true 
+    }
+];
+
+export default class ProviderResult extends LightningElement {
+
+    @api recordId;
+
+    @wire(MessageContext)
+    messageContext;
+
+    providersData = [];
+
+    resultLength = 0;
+    columns = COLS;
+    subscription = null;
+    showResult = true;
+
+
+}
